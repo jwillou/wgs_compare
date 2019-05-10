f390f9340e611d0abd7c1a28a25195ca37010788
willoughby@purdue.edu <willoughby@purdue.edu>
2019-05-10 15:26:56 -0400

    update mt divergence est

--- calc_div_mtblast.R
+++ calc_div_mtblast.R
@@ -1,18 +1,19 @@
-setwd("/scratch/snyder/j//jwillou/wgs/compare/dxy/")
-setwd("/Volumes/jwillou/wgs/compare/dxy/")
+setwd("/scratch/snyder/j//jwillou/wgs/compare/divergence/")
+#setwd("/Volumes/jwillou/wgs/compare/divergence/")
 
 library(ape)
-directories = read.table("../blast_21/compare.txt", sep="\t")
-colnames(directories) = c("sp1", "sp2", "class")
+directories = read.table("../mtblast_21/mtDNAcompare.txt", sep="\t")
+colnames(directories) = c("class", "sp1", "sp2")
 directories$combined = paste(directories$sp1, directories$sp2, sep="_")
 
 #iterate over directories list and estimate divergence
 divOUT = NULL
-for(f in 1:1){ #length(directories)){
+for(f in 1:nrow(directories)){ 
   #read in each data file
-  data = read.table(paste("../blast_21/", directories$combined[f], "/outblast21.txtT", sep=""), sep="\t", header=F)
+  data = read.table(paste("../mtblast_21/", directories$combined[f], "/outblast21.txt", sep=""), sep="\t", header=F)
   colnames(data) = c("qid", "sid", "percid", "alength", "mismatches", "gapopen", "qstart", "qend", "sstart", 
                      "send", "e", "bitscore", "nident", "mismatch", "qseq", "sseq")
+  
   #prep aligned sequences
   q = strsplit(paste(data$qseq, "", collapse=""), "")
   s = strsplit(paste(data$sseq, "", collapse=""), "")
@@ -31,4 +32,4 @@ for(f in 1:1){ #length(directories)){
 
 divOUT = as.data.frame(divOUT)
 colnames(divOUT) = c("spp", "length", "raw", "K80", "K81", "F84", "TN93")
-
+write.table(divOUT, "mtDNAdiv_estimates.csv", sep=",", row.names=F, col.names=T)
